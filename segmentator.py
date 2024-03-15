2de28c248a3c07ddf3b5d054d755e19f2b67be15
NaumenkoRoman <roman.naumenko@ucu.edu.ua>
2024-03-15 22:36:27 +0200

    Added LangSAM segmentator backend.

--- segmentator.py
+++ segmentator.py
@@ -3,11 +3,21 @@ import torch
 from PIL import Image
 import cv2
 from FastSAM.fastsam import FastSAM, FastSAMPrompt
+from lang_sam import LangSAM
+from enum import Enum, auto
+
+class SegmentBackend(Enum):
+    FastSAM = auto()
+    LangSAM = auto()
 
 class Segmentator:
-    def __init__(self, model_path: str = "FastSAM.pt", config: dict = None):
+    def __init__(self, backend: SegmentBackend = SegmentBackend.LangSAM, model_path: str = "FastSAM.pt", config: dict = None):
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.segmentation_model = FastSAM(model_path)#.to(self.device)
+        self.backend = backend
+        if backend == SegmentBackend.FastSAM:
+            self.model = FastSAM(model_path)#.to(self.device)
+        else:
+            self.model = LangSAM()
 
         default_config = {
             'imgsz': 1024,
@@ -18,9 +28,22 @@ class Segmentator:
 
         # Update defaults with any configurations passed to constructor
         self.config = {**default_config, **(config if config is not None else {})}
-
-    def generate_masks(self, image_path: str, prompts: list):
-        everything_results = self.segmentation_model(
+    
+    def get_masks(self, image_path: str, prompts: list):
+        if self.backend == SegmentBackend.LangSAM:
+            return self._get_masks_langsam(image_path, prompts)
+        else:
+            return self._get_masks_fastsam(image_path, prompts)
+        
+    def _get_masks_langsam(self, image_path: str, prompts: list):
+        masks = []
+        for prompt in prompts:
+            mask, _, _, _ = self.model.predict(original_image, prompt)
+            masks.append(mask[0].numpy().astype(np.uint8))
+        return masks
+    
+    def _get_masks_fastsam(self, image_path: str, prompts: list):
+        everything_results = self.model(
             image_path,
             device=self.device,
             retina_masks=True,
@@ -31,7 +54,6 @@ class Segmentator:
         prompt_process = FastSAMPrompt(image_path, everything_results, device=self.device)
         masks = []
         for prompt in prompts:
-            print(prompt)
             raw_mask = prompt_process.text_prompt(text=prompt)[0]
             kernel = np.ones(self.config['dilation_kernel'], np.uint8)
             dilated_mask = cv2.dilate(raw_mask.astype(np.uint8), kernel, iterations=1)
